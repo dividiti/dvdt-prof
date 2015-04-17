2254b4f7ef24762b305d4a869aec11c5ba99a0ad
Anton Lokhmotov <anton@dividiti.com>
2015-04-17 22:51:05 +0000

    Cached poiners to original OpenCL functions.

--- cxx/prof.cpp
+++ cxx/prof.cpp
@@ -30,7 +30,7 @@ static const size_t default_local_work_size = 1;
 static const size_t null_local_work_size = 0;
 
 
-cl_context cached_context = NULL;
+static cl_context cached_context = NULL;
 
 
 namespace dvdt {
@@ -68,9 +68,18 @@ output_profiling_info(const char * call, cl_event * prof_event)
 // - clCreateCommandQueue()
 // - clBuildProgram()
 // - clCreateKernel()
+//
 // - clEnqueueNDRangeKernel()
 // - clEnqueueReadBuffer()
+// - clEnqueueWriteBuffer()
 //
+static clCreateCommandQueue_type clCreateCommandQueue_original = NULL;
+static clBuildProgram_type clBuildProgram_original = NULL;
+static clCreateKernel_type clCreateKernel_original = NULL;
+static clEnqueueNDRangeKernel_type clEnqueueNDRangeKernel_original = NULL;
+static clEnqueueReadBuffer_type clEnqueueReadBuffer_original = NULL;
+static clEnqueueWriteBuffer_type clEnqueueWriteBuffer_original = NULL;
+
 
 // https://www.khronos.org/registry/cl/sdk/1.2/docs/man/xhtml/clCreateCommandQueue.html
 extern CL_API_ENTRY cl_command_queue CL_API_CALL
@@ -86,6 +95,11 @@ clCreateCommandQueue(
     // API call.
     const char * call = "clCreateCommandQueue";
     std::cout << prefix << sep << call << lf;
+
+    if (NULL == clCreateCommandQueue_original)
+    {
+        clCreateCommandQueue_original = (clCreateCommandQueue_type) dlsym(RTLD_NEXT, call);
+    }
 
     if (NULL == cached_context)
     {
@@ -109,7 +123,6 @@ clCreateCommandQueue(
     std::cout << prefix << sep << call << sep << "start" << sep << boost::posix_time::to_iso_extended_string(start) << lf;
 
     // Original call.
-    clCreateCommandQueue_type clCreateCommandQueue_original = (clCreateCommandQueue_type) dlsym(RTLD_NEXT, call);
     queue = clCreateCommandQueue_original(context, device, properties | CL_QUEUE_PROFILING_ENABLE, errcode_ret);
     
     // End timestamp.
@@ -142,6 +155,11 @@ clBuildProgram(
     const char * call = "clBuildProgram";
     std::cout << prefix << sep << call << lf;
 
+    if (NULL == clBuildProgram_original)
+    {
+        clBuildProgram_original = (clBuildProgram_type) dlsym(RTLD_NEXT, call);
+    }
+
     // Arguments.
     std::cout << prefix << sep << call << sep << "program" << sep << FIXED_WIDTH_PTR(program) << lf;
     // TODO: num_devices.
@@ -161,7 +179,6 @@ clBuildProgram(
     std::cout << prefix << sep << call << sep << "start" << sep << boost::posix_time::to_iso_extended_string(start) << lf;
 
     // Original call.
-    clBuildProgram_type clBuildProgram_original = (clBuildProgram_type) dlsym(RTLD_NEXT, call);
     errcode = clBuildProgram_original(program, num_devices, device_list, options, pfn_notify, user_data);
     // TODO: Make the call blocking so (end - start) represents the actual program build time.
 
@@ -192,6 +209,11 @@ clCreateKernel(
     const char * call = "clCreateKernel";
     std::cout << prefix << sep << call << lf;
 
+    if (NULL == clCreateKernel_original)
+    {
+        clCreateKernel_original = (clCreateKernel_type) dlsym(RTLD_NEXT, call);
+    }
+
     // Arguments.
     std::cout << prefix << sep << call << sep << "program" << sep << FIXED_WIDTH_PTR(program) << lf;
     std::cout << prefix << sep << call << sep << "name" << sep << kernel_name << lf;
@@ -208,7 +230,6 @@ clCreateKernel(
     std::cout << prefix << sep << call << sep << "start" << sep << boost::posix_time::to_iso_extended_string(start) << lf;
 
     // Original call.
-    clCreateKernel_type clCreateKernel_original = (clCreateKernel_type) dlsym(RTLD_NEXT, call);
     kernel = clCreateKernel_original(program, kernel_name, errcode_ret);
 
     // End timestamp.
@@ -243,6 +264,11 @@ clEnqueueNDRangeKernel(
     // API call.
     const char * call = "clEnqueueNDRangeKernel";
     std::cout << prefix << sep << call << lf;
+
+    if (NULL == clEnqueueNDRangeKernel_original)
+    {
+        clEnqueueNDRangeKernel_original = (clEnqueueNDRangeKernel_type) dlsym(RTLD_NEXT, call);
+    }
 
     // Arguments.
     std::cout << prefix << sep << call << sep << "queue"  << sep << FIXED_WIDTH_PTR(queue) << lf;
@@ -307,7 +333,6 @@ clEnqueueNDRangeKernel(
     cl_event * prof_event = (NULL != event ? event : &prof_event_obj);
 
     // Original call.
-    clEnqueueNDRangeKernel_type clEnqueueNDRangeKernel_original = (clEnqueueNDRangeKernel_type) dlsym(RTLD_NEXT, call);
     errcode = clEnqueueNDRangeKernel_original(queue, kernel, work_dim, global_work_offset, global_work_size, local_work_size,
         num_events_in_wait_list, event_wait_list, prof_event);
     if (CL_SUCCESS != errcode)
@@ -350,6 +375,11 @@ clEnqueueReadBuffer(
     const char * call = "clEnqueueReadBuffer";
     std::cout << prefix << sep << call << lf;
 
+    if (NULL == clEnqueueReadBuffer_original)
+    {
+        clEnqueueReadBuffer_original = (clEnqueueReadBuffer_type) dlsym(RTLD_NEXT, call);
+    }
+
     // Arguments.
     std::cout << prefix << sep << call << sep << "queue"  << sep << FIXED_WIDTH_PTR(queue) << lf;
     std::cout << prefix << sep << call << sep << "buffer" << sep << FIXED_WIDTH_PTR(buffer) << lf;
@@ -382,7 +412,6 @@ clEnqueueReadBuffer(
     cl_event * prof_event = (NULL != event ? event : &prof_event_obj);
 
     // Original call.
-    clEnqueueReadBuffer_type clEnqueueReadBuffer_original = (clEnqueueReadBuffer_type) dlsym(RTLD_NEXT, call);
     errcode = clEnqueueReadBuffer_original(queue, buffer, blocking, offset, size, ptr,
         num_events_in_wait_list, event_wait_list, prof_event);
     if (CL_SUCCESS != errcode)
@@ -425,6 +454,11 @@ clEnqueueWriteBuffer(
     const char * call = "clEnqueueWriteBuffer";
     std::cout << prefix << sep << call << lf;
 
+    if (NULL == clEnqueueWriteBuffer_original)
+    {
+        clEnqueueWriteBuffer_original = (clEnqueueWriteBuffer_type) dlsym(RTLD_NEXT, call);
+    }
+
     // Arguments.
     std::cout << prefix << sep << call << sep << "queue"  << sep << FIXED_WIDTH_PTR(queue) << lf;
     std::cout << prefix << sep << call << sep << "buffer" << sep << FIXED_WIDTH_PTR(buffer) << lf;
@@ -457,7 +491,6 @@ clEnqueueWriteBuffer(
     cl_event * prof_event = (NULL != event ? event : &prof_event_obj);
 
     // Original call.
-    clEnqueueWriteBuffer_type clEnqueueWriteBuffer_original = (clEnqueueWriteBuffer_type) dlsym(RTLD_NEXT, call);
     errcode = clEnqueueWriteBuffer_original(queue, buffer, blocking, offset, size, ptr,
         num_events_in_wait_list, event_wait_list, prof_event);
     if (CL_SUCCESS != errcode)
