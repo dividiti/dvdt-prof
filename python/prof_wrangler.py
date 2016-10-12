14b88e9d2a59f045941dfde6d7d20a0b00a10c65
Anton Lokhmotov <anton@dividiti.com>
2016-10-12 17:51:08 +0000

    Return simple DataFrame with timing differences.

--- python/prof_wrangler.py
+++ python/prof_wrangler.py
@@ -7,6 +7,8 @@ import json
 
 import dateutil.parser
 
+import pandas as pd
+
 def test():
     print ("prof_wrangler.py")
 
@@ -26,7 +28,7 @@ def ts_delta_ns(ts_end, ts_start):
 def index_calls(trace):
     indexed_trace = [
         dict(trace, call_index=index)
-        for trace, index in zip(trace, range(len(trace))) 
+        for trace, index in zip(trace, range(len(trace)))
     ]
     return indexed_trace
 
@@ -38,3 +40,32 @@ def filter_calls(trace, call_names):
         call for call in trace if call['call'] in call_names
     ]
     return filtered_trace
+
+# Return a DataFrame containing the differences between the profiling markers
+# and the timestamps in nanoseconds for a trace with any enqueues.
+def df_enqueues_ns(nqs, label_fn = lambda nq: '%s' % str(nq['call_index'])) :
+    def _df_data():
+	data = [
+            {
+                'p1 - p0' : nq['profiling']['submit'] - nq['profiling']['queued'], # command queueing time
+                'p2 - p1' : nq['profiling']['start']  - nq['profiling']['submit'], # job queueing time
+                'p3 - p2' : nq['profiling']['end']    - nq['profiling']['start'],  # kernel execution time
+                'p3 - p0' : nq['profiling']['end']    - nq['profiling']['queued'], # total execution time
+                't1 - t0' : ts_delta_ns(ts_end=nq['timestamp']['end'], ts_start=nq['timestamp']['start']), # chrono time
+                't1 - t0 - (p3 - p0)' :
+                            ts_delta_ns(ts_end=nq['timestamp']['end'], ts_start=nq['timestamp']['start']) -
+                            nq['profiling']['end'] + nq['profiling']['queued'],    # chrono overhead
+            }
+            for nq in nqs
+        ]
+        return data
+
+    def _df_index():
+        index = pd.MultiIndex.from_tuples(
+            names=('label', 'call'),
+            tuples=[ (label_fn(nq), nq['call']) for nq in nqs ]
+        )
+        return index
+
+    df = pd.DataFrame(data=_df_data(),index=_df_index())
+    return df
