86588695f117bc2fb3d4b58322761a2403d5d6f3
Anton Lokhmotov <anton@dividiti.com>
2017-02-09 15:03:04 +0000

    Update clBuildProgram (tests, no parsing).

--- tests/clBuildProgram.cpp
+++ tests/clBuildProgram.cpp
@@ -1,14 +1,16 @@
 #include <CL/opencl.h>
 #include <cassert>
 
+typedef void (CL_CALLBACK pfn_notify_t)(cl_program program, void * user_data);
+
 int main()
 {
     cl_program program = (cl_program) 0x01234567;
-    cl_uint num_devices = 0;
-    const cl_device_id * device_list = NULL;
+    const cl_uint num_devices = 2;
+    cl_device_id device_list[2] = { (cl_device_id) 0x12345678, (cl_device_id) 0x23456789 };
     const char * options = "-Werror -DN=1024";
-    void (CL_CALLBACK * pfn_notify)(cl_program program, void * user_data) = NULL;
-    void * user_data = NULL;
+    pfn_notify_t * pfn_notify = (pfn_notify_t *) 0x3456789a;
+    void * user_data = (void *) 0x456789ab;
 
     cl_int errcode = clBuildProgram(program, num_devices, device_list, options, pfn_notify, user_data);
     assert(CL_SUCCESS == errcode);
--- tests/clBuildProgram.py
+++ tests/clBuildProgram.py
@@ -14,14 +14,32 @@ call = 'clBuildProgram'
 _id  = ''
 print '%s%s' % (call, _id)
 
+# FIXME: taken from clBuildProgram.py - avoid duplication.
+# Parse initialisation list of form: lhs = { elem, ... }.
+def match_init_list(text, lhs_regex, elem_regex):
+    result = []
+    match = re.search('%s(\s*)=(\s*)\{(\s*)(?P<elem>%s)' % (lhs_regex, elem_regex), text)
+    while match and match.group('elem') != '}':
+        result.append(match.group('elem'))
+        text = text[match.end():]
+        match = re.search('(?P<elem>%s|\})' % elem_regex, text)
+    return result
+
 # Parse test source.
 source = {}
 with open(call + _id + '.cpp', 'r') as f:
     source['text'] = f.read()
     source['program'] = re.search('\(cl_program\) (?P<program>%s)' %
                                   ptr_regex, source['text']).group('program')
-    source['options'] = re.search('options = \"(?P<options>%s)\"' %
-                                  opts_regex, source['text']).group('options')
+
+    num_events = int(re.search('num_devices(\s*)=(\s*)(?P<num_devices>\d+)', source['text']).group('num_devices'))
+    cl_device_ptr_list = match_init_list(source['text'], 'device_list\[%d\]' % num_events, '\(cl_device_id\) %s' % ptr_regex)
+    source['device_list'] = [re.match('\(cl_device_id\) (?P<ptr>%s)' % ptr_regex, cl_device_ptr).group('ptr') for cl_device_ptr in cl_device_ptr_list]
+
+    source['options'] = re.search('options(\s*)=(\s*)\"(?P<options>%s)\"' % opts_regex, source['text']).group('options')
+
+    source['pfn_notify'] = re.search('\(pfn_notify_t \*\) (?P<pfn_notify>%s)' % ptr_regex, source['text']).group('pfn_notify')
+    source['user_data'] = re.search('\(void \*\) (?P<user_data>%s)' % ptr_regex, source['text']).group('user_data')
 
 # Read from stdin (via pipe).
 output = sys.stdin.read()
@@ -35,7 +53,11 @@ print
 
 status = True
 status &= (source['program'] == result['program'])
+status &= (cmp(source['device_list'], result['device_list']) == 0)
 status &= (source['options'] == result['options'])
+status &= (source['pfn_notify'].lower() == result['pfn_notify'].lower())
+status &= (source['user_data'].lower() == result['user_data'].lower())
+status &= (0 == result['errcode'])
 
 print '%s%s: %s' % (call, _id, 'PASSED' if status else 'FAILED')
 print
